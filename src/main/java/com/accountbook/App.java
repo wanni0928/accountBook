fe6b6a50387efb6b758ea75673a8d855be94d99e
wanni0928 <chlgudwnd123@gmail.com>
2020-10-19 17:06:33 +0900

    Update App.java

--- src/main/java/com/accountbook/App.java
+++ src/main/java/com/accountbook/App.java
@@ -1,8 +1,6 @@
 package com.accountbook;
 
 import com.accountbook.controller.AccountController;
-import com.accountbook.repository.AccountRepository;
-import com.accountbook.service.AccountService;
 
 import java.io.IOException;
 
@@ -10,8 +8,6 @@ public class App {
     public static void main(String[] args) throws IOException {
         // AppConfig
         AppConfig appConfig = new AppConfig();
-        AccountRepository accountRepository = appConfig.getAccountRepository();
-        AccountService accountService = appConfig.getAccountService();
         AccountController accountController = appConfig.getAccountController();
 
 //        accountController.updateAccount(1L);
