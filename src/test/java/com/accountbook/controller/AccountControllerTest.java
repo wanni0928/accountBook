547ea01b472eb5626020d3e466935af3239d9b68
wanni0928 <chlgudwnd123@gmail.com>
2020-10-20 14:50:47 +0900

    Create AccountControllerTest.java

---
+++ src/test/java/com/accountbook/controller/AccountControllerTest.java
@@ -0,0 +1,28 @@
+package com.accountbook.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountControllerTest {
+
+    @Test
+    void addExpand() {
+    }
+
+    @Test
+    void addIncome() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void updateAccount() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
