4525d1c949bd55a017896fef7ab02ba0054636c9
wanni0928 <chlgudwnd123@gmail.com>
2020-10-20 14:50:22 +0900

    Update Account.java

--- src/main/java/com/accountbook/domain/Account.java
+++ src/main/java/com/accountbook/domain/Account.java
@@ -8,36 +8,13 @@ import java.time.LocalDateTime;
 @Getter @Setter
 public class Account {
     private Long accountId;
+    private Long categoryId;
     private String accountTitle;
     private String accountContent;
     private int accountBalance;
     private String accountStatus;
-    private String category;
     private LocalDateTime accountDate;
 
     public Account() {
-    }
-
-    public Account(Long accountId, String accountTitle, String accountContent, int accountBalance, String accountStatus, String category, LocalDateTime accountDate) {
-        this.accountId = accountId;
-        this.accountTitle = accountTitle;
-        this.accountContent = accountContent;
-        this.accountBalance = accountBalance;
-        this.accountStatus = accountStatus;
-        this.category = category;
-        this.accountDate = accountDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Account{" +
-                "accountId=" + accountId +
-                ", accountTitle='" + accountTitle + '\'' +
-                ", accountContent='" + accountContent + '\'' +
-                ", accountBalance=" + accountBalance +
-                ", accountStatus='" + accountStatus + '\'' +
-                ", category='" + category + '\'' +
-                ", accountDate=" + accountDate +
-                '}';
     }
 }
