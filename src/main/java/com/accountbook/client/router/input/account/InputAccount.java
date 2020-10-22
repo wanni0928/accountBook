fed95d5a6e98e782faf270bb49bc3cd11dd527db
wanni0928 <chlgudwnd123@gmail.com>
2020-10-22 19:38:55 +0900

    Update InputAccount.java

--- src/main/java/com/accountbook/client/router/input/account/InputAccount.java
+++ src/main/java/com/accountbook/client/router/input/account/InputAccount.java
@@ -62,30 +62,31 @@ public class InputAccount implements Create, Delete, Update {
     public void showInputAccount() {
         String input = "";
         String guidance =
-                "\n<가계부 입력페이지> 이용하고 싶은 메뉴 번호를 입력 해주세요.\n" +
+                "\n<가계부 입력페이지> 이용하고 싶은 메뉴 번호를 입력 해주세요. 메뉴를 나가고 싶으시면 -1을 선택하세요.\n" +
                 "1. 추가하기\n" +
                         "2. 수정하기\n" +
                         "3. 삭제하기\n";
 
-        System.out.println(guidance);
 
-        while (Integer.parseInt(input) != -1){
+        inputAccountMenu:
+        while (true){
+            System.out.println(guidance);
             input = scanner.nextLine();
-            switch (Integer.parseInt(input)){
+            switch (input){
                 // quit
-                case -1:
+                case "-1":
                     System.out.println("<가계부 입력페이지> 종료");
-                    break;
+                    break inputAccountMenu;
                 // create
-                case 1:
+                case "1":
                     showCreateAccount();
                     break;
                 // update
-                case 2:
+                case "2":
                     showUpdateAccount();
                     break;
                 // delete
-                case 3:
+                case "3":
                     showDeleteForm();
                     break;
                 // handling exception
@@ -100,16 +101,20 @@ public class InputAccount implements Create, Delete, Update {
         String input = "";
         Account account;
         accounts = (List<Account>) cache.getCache().get(DomainType.ACCOUNT.getKey());
-        while (!input.equals("q")) {
-            System.out.println("삭제하기를 선택하셨네요. 뭘 원하십니까? 숫자로 쓰세요.");
+        deleteAccount:
+        while (true) {
+            System.out.println("삭제하기를 선택하셨네요. 뭘 원하십니까? 숫자로 쓰세요. 해당 메뉴를 취소하고 싶으면 -1을 입력하세요");
             System.out.println("1. 작성 목록 확인하기");
             System.out.println("2. 가계부 삭제하기");
             input = scanner.nextLine();
-            switch (Integer.parseInt(input)){
-                case 1:
+            switch (input){
+                case "-1":
+                    System.out.println("삭제하기를 종료 합니다.");
+                    break deleteAccount;
+                case "1":
                     showAccountList(accounts);
                     break;
-                case 2:
+                case "2":
                     System.out.println("삭제 하실 가계부 번호를 쓰세요");
                     Long id = Long.parseLong(scanner.nextLine());
                     account = accountController.findById(id);
@@ -120,7 +125,10 @@ public class InputAccount implements Create, Delete, Update {
                         System.out.println("내용 : " + account.getAccountContent());
                         System.out.println("액수 : " + account.getAccountBalance());
                         input = scanner.nextLine();
-                        if(input.equals("q")) delete(id);
+                        if(input.equals("q")) {
+                            delete(id);
+                            break deleteAccount;
+                        };
                     }else {
                         System.out.println("없는걸 어떻게 지워요 다시해요");
                     }
@@ -139,17 +147,20 @@ public class InputAccount implements Create, Delete, Update {
         Account account;
         accounts = (List<Account>) cache.getCache().get(DomainType.ACCOUNT.getKey());
         categories = (List<Category>) cache.getCache().get(DomainType.CATEGORY.getKey());
-
-        while (!input.equals("q")) {
-            System.out.println("수정하기를 선택하셨습니다. 원하시는 기능을 번호로 입력 해주세요.");
+        updateAccount:
+        while (!input.equals(-1)) {
+            System.out.println("수정하기를 선택하셨습니다. 원하시는 기능을 번호로 입력 해주세요. 해당 메뉴를 취고하고 싶으면 -1을 입력하세요.");
             System.out.println("1. 작성 목록 확인하기");
             System.out.println("2. 가계부 수정 하기");
             input = scanner.nextLine();
-            switch (Integer.parseInt(input)){
-                case 1:
+            switch (input){
+                case "-1":
+                    System.out.println("수정하기를 종료 합니다.");
+                    break updateAccount;
+                case "1":
                     showAccountList(accounts);
                     break;
-                case 2:
+                case "2":
                     System.out.println("수정하고 싶은 게시글 번호를 입력하세요.");
                     input = scanner.nextLine();
                     account = accountController.findById(Long.parseLong(input));
@@ -168,19 +179,19 @@ public class InputAccount implements Create, Delete, Update {
                         System.out.println(keyWord + "로 정하셨습니다. 기존 내용을 유지하고 싶은 부분이 있다면, 그냥 엔터를 치세요.");
 
                         showCategoryList(categories, keyWord);
-                        System.out.println("수정하고 싶은 카테고리 코드를 입력하세요. (기존내용 : " + account.getCategoryId());
+                        System.out.println("수정하고 싶은 카테고리 코드를 입력하세요. (기존내용 : " + account.getCategoryId() + ")");
                         input = scanner.nextLine();
                         Long categoryId = input.equals("") ? account.getCategoryId() : Long.parseLong(input);
 
-                        System.out.println("제목은 무엇으로 하시겠습니까?(기존내용 : " + account.getAccountTitle());
+                        System.out.println("제목은 무엇으로 하시겠습니까?(기존내용 : " + account.getAccountTitle() + ")");
                         input = scanner.nextLine();
                         String accountTitle = input.equals("") ? account.getAccountTitle() : input;
 
-                        System.out.println("내용은 무엇으로 하시겠습니까?(기존내용 : " + account.getAccountContent());
+                        System.out.println("내용은 무엇으로 하시겠습니까?(기존내용 : " + account.getAccountContent() + ")");
                         input = scanner.nextLine();
                         String accountContent = input.equals("") ? account.getAccountContent() : input;
 
-                        System.out.println("액수는 얼마로 고치시겠습니까?(기존내용 : " + account.getAccountBalance());
+                        System.out.println("액수는 얼마로 고치시겠습니까?(기존내용 : " + account.getAccountBalance() + ")");
                         input = scanner.nextLine();
                         int accountBalance = input.equals("") ? account.getAccountBalance() : Integer.parseInt(input);
 
@@ -195,14 +206,17 @@ public class InputAccount implements Create, Delete, Update {
 
                         System.out.println("이 내용이 맞습니까? 맞으면 q를 입력하시고, 다시 입력하시려면 아무키나 눌러주세요.");
                         input = scanner.nextLine();
-                        if(input.equals("q")) update(account, accountForm);
+                        if(input.equals("q")) {
+                            update(account, accountForm);
+                            break updateAccount;
+                        }
                     }else {
                         System.out.println("없는 걸 어떻게 수정해요? 처음부터 다시 진행하세요.");
+                        continue;
                     }
                     break;
             }
         }
-        ;
     }
 
     private void showAccountList(List<Account> accountList) {
@@ -224,17 +238,22 @@ public class InputAccount implements Create, Delete, Update {
                 "STEP4. 액수를 입력 하세요"
         };
 
-        while (!input.equals("q")) {
+        label:
+        while (true) {
             System.out.println("추가하기를 선택하셨습니다.");
-            System.out.println("1.수입 2.지출 (번호로 작성, 해당 페이지를 벗어나고 싶으면 q를 입력하세요.)");
+            System.out.println("1.수입 2.소비 (번호로 작성, 해당 페이지를 벗어나고 싶으면 -1를 입력하세요.)");
            input = scanner.nextLine();
-            if(input.equals("1")) {
-                keyWord = "수입";
-            } else if(input.equals("2")){
-                keyWord = "소비";
-            } else {
-                System.out.println("뭐하세요? 처음부터 다시 쓰세요.");
-                continue;
+
+            switch (input) {
+                case "1":
+                    keyWord = "수입";
+                    break;
+                case "2":
+                    keyWord = "소비";
+                    break;
+                case "-1":
+                    System.out.println("추가하기를 취소 합니다.");
+                    break label;
             }
 
             for (int i = 0; i < steps.length;) {
@@ -258,10 +277,16 @@ public class InputAccount implements Create, Delete, Update {
 
             System.out.println("이 내용이 맞습니까? 맞으면 q를 입력하시고, 다시 입력하시려면 아무키나 눌러주세요.");
             input = scanner.nextLine();
+
+            if(input.equals("q")){
+                System.out.println("입력하기를 종료하고 저장합니다.");
+                accountForm.createAccountForm(formValues);
+                save(accountForm);
+                break;
+            }
         }
-        System.out.println("입력하기를 종료하고 저장합니다.");
-        accountForm.createAccountForm(formValues);
-        save(accountForm);
+
+
     }
 
     private void showCategoryList(List<Category> categories, String finalKeyWord) {
